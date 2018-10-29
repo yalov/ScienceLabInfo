b4e502e9b89b81d599a1c4b969915a6f58ec3948
yalov <alexander.yalov@gmail.com>
2018-10-29 21:41:47 +0300

    Initial commit

---
+++ ScienceLabInfo/ScienceLabEditor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KSP.Localization;
+
+namespace ScienceLabInfo
+{
+    [KSPAddon(KSPAddon.Startup.EditorAny, false)]
+    public class ScienceLabEditor : MonoBehaviour
+    {
+        private void Start()
+        {
+            List<AvailablePart> parts = PartLoader.LoadedPartsList.Where
+                    (p => p.partPrefab.Modules.GetModules<ModuleScienceConverter>().Any() &&
+                        p.partPrefab.Modules.GetModules<ModuleScienceLab>().Any()).ToList();
+
+            foreach (AvailablePart part in parts)
+            {
+                List<ModuleScienceConverter> modulesSC = part.partPrefab.Modules.GetModules<ModuleScienceConverter>();
+                List<ModuleScienceLab> modulesSL = part.partPrefab.Modules.GetModules<ModuleScienceLab>();
+
+                if (modulesSC.Count != 1) continue;
+                if (modulesSL.Count != 1) continue;
+
+                ModuleScienceConverter moduleSC = modulesSC[0];
+                ModuleScienceLab moduleSL = modulesSL[0];
+
+                moduleSL.Fields.ToString();
+
+                List<AvailablePart.ModuleInfo> modinfos = part.moduleInfos;
+
+                foreach (AvailablePart.ModuleInfo modinfo in modinfos)
+                {
+                    if (modinfo.moduleName == moduleSL.GUIName)
+                    {
+
+                        double lab_modificator = moduleSC.dataProcessingMultiplier / 0.5 * Math.Pow(10, 7 - moduleSC.researchTime);
+
+                        modinfo.info = 
+                              Localizer.Format("#SLI_ScientistsRequired", moduleSL.crewsRequired)
+                            + Localizer.Format("#SLI_PowerUsage", moduleSC.powerRequirement)
+                            + Localizer.Format("#SLI_DataStorage", moduleSL.dataStorage)
+                            + Localizer.Format("#SLI_ScienceStorage", moduleSC.scienceCap)
+                            + Localizer.Format("#SLI_DatatoScience", moduleSC.scienceMultiplier)
+
+                            + Localizer.Format("#SLI_ResearchSpeed")
+                            + " " + Localizer.Format("#SLI_TheLabModificator", lab_modificator.ToString("#0.0#"))
+                            + " " + Localizer.Format("#SLI_ScientistLevelBonus", (moduleSC.scientistBonus).ToString("+#0%;-#0%"))
+
+                            + Localizer.Format("#SLI_DataBonuses")
+                            + " " + Localizer.Format("#SLI_SurfaceBonus", moduleSL.SurfaceBonus.ToString("+#0%;-#0%"))
+                            + " " + Localizer.Format("#SLI_ContextBonus", moduleSL.ContextBonus.ToString("+#0%;-#0%"))
+                            + " " + Localizer.Format("#SLI_HomeworldBonus", (moduleSL.homeworldMultiplier - 1).ToString("+#0%;-#0%"));
+                        
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ScienceLabInfo/ScienceLabFlight.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ScienceLabInfo
+{
+    public class ModuleScienceInfo : PartModule
+    {
+        [KSPField(guiActive = true, guiName = "#SLI_ScientistsRateModificator")]
+        string ScientistsModificator_str;
+
+        [KSPField(guiActive = false, guiName = "#SLI_LabRateModificator", advancedTweakable = true)]
+        string LabModificator_str;
+
+        [KSPField(guiActive = true, guiName = "#SLI_DatatoScienceConversion", advancedTweakable = true)]
+        string DataScienceConversion_str;
+
+        ModuleScienceConverter msc;
+
+        public void Start()
+        {
+            msc = part.Modules.GetModule<ModuleScienceConverter>();
+
+            DataScienceConversion_str = "1:" + msc.scienceMultiplier;
+
+            double labModificator = msc.dataProcessingMultiplier / 0.5 * Math.Pow(10, 7 - msc.researchTime);
+
+            if (labModificator != 1.0)
+            {
+                Fields["LabModificator_str"].guiActive = true;
+                LabModificator_str = String.Format("×{0:F2}", labModificator);
+            }
+        }
+
+        public void Update()
+        {
+            int Stars = 0;
+            int ScientistCount = 0;
+            foreach (ProtoCrewMember crewmember in part.protoModuleCrew)
+            {
+                if (crewmember.experienceTrait.Config.Name == "Scientist")
+                {
+                    Stars += crewmember.experienceLevel;
+                    ScientistCount++;
+                }
+            }
+
+            ScientistsModificator_str = String.Format("×{0:F2}", ScientistCount + Stars * msc.scientistBonus);
+        }
+    }
+}
